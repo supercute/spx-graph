438bcdad3dc20e005763f1d4b16cd8c7c72917dd
Danil Shumskikh <d@shumskikh.ru>
2025-09-16 18:45:39 +0300

    change comments language in types

--- internal/spx/types.go
+++ internal/spx/types.go
@@ -5,23 +5,23 @@ import "time"
 type Event struct {
 	FunctionID int   `json:"function_id"`
 	EventType  int   `json:"event_type"` // 1 = enter, 0 = exit
-	Time       int64 `json:"time"`       // микросекунды
-	Memory     int64 `json:"memory"`     // байты
+	Time       int64 `json:"time"`       // microseconds
+	Memory     int64 `json:"memory"`     // bytes
 }
 
 type Profile struct {
 	Events    []Event        `json:"events"`
-	Functions map[int]string `json:"functions"` // ID -> имя функции
+	Functions map[int]string `json:"functions"` // ID -> function name
 }
 
 type CallInfo struct {
 	FunctionID  int           `json:"function_id"`
-	Parent      *int          `json:"parent"`       // ID родительской функции
-	Duration    time.Duration `json:"duration"`     // длительность выполнения
-	MemoryDelta int64         `json:"memory_delta"` // изменение памяти
+	Parent      *int          `json:"parent"`       // parent function ID
+	Duration    time.Duration `json:"duration"`     // execution duration
+	MemoryDelta int64         `json:"memory_delta"` // memory change
 	StartTime   int64         `json:"start_time"`
 	EndTime     int64         `json:"end_time"`
-	Children    []int         `json:"children"` // ID дочерних функций
+	Children    []int         `json:"children"` // child function IDs
 }
 
 type CallGraph struct {
@@ -37,7 +37,7 @@ type CallNode struct {
 	SelfDuration   time.Duration `json:"self_duration"`
 	CallCount      int           `json:"call_count"`
 	TotalMemory    int64         `json:"total_memory"`
-	Percentage     float64       `json:"percentage"` // процент от общего времени
+	Percentage     float64       `json:"percentage"` // percentage of total time
 	SelfPercentage float64       `json:"self_percentage"`
 }
 
